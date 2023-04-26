6f5b203d4c034600c117623175d7ef2595b051f7
unknown <umarvayevxusan@gmail.com>
2023-04-26 10:32:15 +0500

    Main page

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function main()
+    {
+        return view('main');
+    }
+    public function welcome()
+    {
+        return view('welcome', ['name' =>"<script>alert('salom dostim')</script>"]);
+    }
+}
